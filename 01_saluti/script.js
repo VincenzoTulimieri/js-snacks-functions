8f23b09a4d5df9f972abdee13a1ad14dc23fed5c
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-03 19:26:44 +0100

    feat: esercizio 1 fatto

---
+++ 01_saluti/script.js
@@ -0,0 +1,22 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+
+// Dichiara la funzione qui.
+
+function greetings() { 
+    return 'ciao'
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+greetings()
+const hi = greetings() 
+
+const hello = hi + ' ' + userName
+console.log(hello)
+
+
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
